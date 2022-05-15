2502544f206d1212abf6f5dbe4b8815158c30e85
Jorge <jorge@gmail.com>
2022-05-14 23:48:10 -0600

    Modificaciones en la vista inicio ofertas laborales

--- apps/OfertasLaborales/views.py
+++ apps/OfertasLaborales/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView, DetailView
 from django.views.generic.list import MultipleObjectMixin
+from django.contrib.auth.models import User
+from .models import *
+from .forms import *
 
 # Create your views here.
 
-class Inicio(ListView):
+class Inicio(ListView, MultipleObjectMixin):
     
     template_name = 'OfertasLaborales/inicio.html'
+    object_list= (OfertaLaboral, Categoria, Facultad, Sede)
     
     def get(self, request):
-        return render(request, self.template_name)
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        nombres = ('ofertaslaborales', 'categorias', 'facultades', 'sedes')
+        index = 0
+        for objeto in self.object_list:
+            context[nombres[index]] = objeto.objects.all()
+            index +=1
+        return context
+    
--- static/js/acciones.js
+++ static/js/acciones.js
@@ -1,13 +1,24 @@
 
 
 $('#btn-sidebar').click(function() {    
-    $('#sidebar').animate({
-        width: '0',
-    }, 'slow');
+    $('.ventana-bloqueo').animate({opacity: '0',}, 'slow');
+    $('#sidebar').animate({width: '0',}, 'slow', function(){
+        $('.ventana-bloqueo').css('display', 'none');
+    });
+    $('body').css('overflow', 'initial');
+});
+
+$('.ventana-bloqueo').click(function() {    
+    $('.ventana-bloqueo').animate({opacity: '0',}, 'slow');
+    $('#sidebar').animate({width: '0',}, 'slow', function(){
+        $('.ventana-bloqueo').css('display', 'none');
+    });
+    $('body').css('overflow', 'initial');
 });
 
 $('#logo').click(function() {
-    $('#sidebar').animate({
-        width: '250px',
-    }, 'slow');
+    $('body').css('overflow', 'hidden');
+    $('.ventana-bloqueo').css('display', 'initial');
+    $('#sidebar').animate({width: '250px',}, 'slow');
+    $('.ventana-bloqueo').animate({opacity: '.75',}, 'slow');
 });
